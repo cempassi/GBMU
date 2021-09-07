39b76c9e853ed8692cbe5ded0870ccc4ffe5f715
Cedric M'Passi <cedric.mpassi@pmu.fr>
2021-09-07 10:36:17 +0000

    Only create SOC from TryFrom

--- crates/soc/src/soc.rs
+++ crates/soc/src/soc.rs
@@ -1,12 +1,14 @@
 use crate::header::Header;
 use cpu::cpu::Cpu;
-use memory::Memory;
+use memory::{Cartridge, Memory};
 use pretty_hex::*;
 use std::convert::TryFrom;
 use std::fs;
 
 const ROM_START: usize = 0x150;
 const HEADER_START: usize = 0x100;
+const HEADER_LEN: usize = 0x50;
+const HEAD_LEN: usize = 0x100;
 
 pub struct SOC {
     clock: u32,
@@ -14,39 +16,32 @@ pub struct SOC {
     memory: Memory,
 }
 
-impl Default for SOC {
-    fn default() -> Self {
-        SOC::new()
-    }
-}
-
 impl TryFrom<&str> for SOC {
     type Error = std::io::Error;
 
     fn try_from(path: &str) -> Result<Self, Self::Error> {
         let mut head = fs::read(path)?;
-        let _rom = head.split_off(ROM_START);
+        let rom = head.split_off(ROM_START);
         let raw_header = head.split_off(HEADER_START);
-        assert_eq!(head.len(), 0x100);
-        assert_eq!(raw_header.len(), 0x50);
+
+        assert_eq!(head.len(), HEAD_LEN);
+        assert_eq!(raw_header.len(), HEADER_LEN);
 
         println!("head - {:?}", head.hex_dump());
         println!("header - {:?}", raw_header.hex_dump());
+
         let header = Header::try_from(raw_header).expect("Invalid data in raw_header");
-        println!("{:#x?}", header);
-        Ok(SOC::new())
+        //println!("{:#x?}", header);
+
+        let clock: u32 = 0;
+        let cpu: Cpu = Cpu::default();
+        let memory: Memory = Memory::new(header.cartridge, rom);
+
+        Ok(SOC { clock, cpu, memory })
     }
 }
 
 impl SOC {
-    pub fn new() -> Self {
-        SOC {
-            clock: 0,
-            cpu: Cpu::default(),
-            memory: Memory::default(),
-        }
-    }
-
     pub fn run(&mut self) {
         if let Ok(cycles) = self.cpu.step(&mut self.memory) {
             self.clock += cycles
