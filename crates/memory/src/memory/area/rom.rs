590a9c45bae420e0f2547d3f613734ba23497f82
Villat Guillaume <guvillat@student.42.fr>
2021-09-09 20:33:07 +0200

    Feature/mbc1 (#10)
    * debut impl Mbc1
    
    * debut impl Mbc1
    
    * Impl Mbc1
    
    * Finised Mbc1 Impl and test
    
    * Add Const + fct write_ram_bank to clean the code
    
    * fmt code
    
    * Fix ROM import
    
    * Fix ZIP
    
    * Fix clippy warning + format
    
    * Change rom URL
    
    * Format code
    
    * Add const START REG0 et BANK0
    
    * Refacto Mbc1 bank swapping
    
    Co-authored-by: Cedric M'Passi <cempassi@student.42.fr>

--- crates/memory/src/memory/area/rom.rs
+++ crates/memory/src/memory/area/rom.rs
@@ -1,4 +1,5 @@
 pub mod cartridge;
+pub mod mbc1;
 pub mod nombc;
 
 pub use cartridge::Cartridge;
--- crates/memory/src/memory/area/rom/mbc.rs
+++ crates/memory/src/memory/area/rom/mbc.rs
@@ -1,4 +1,9 @@
 #[derive(Debug)]
 pub enum Mbc {
     NoMbc,
+    Mbc1,
+    Mbc2,
+    Mbc3,
+    Mbc4,
+    Mbc5,
 }
