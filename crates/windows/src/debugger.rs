41da76568589581608c5baff6d160d0dfd2f3616
Cedric M'Passi <cempassi@student.42.fr>
2021-09-13 00:34:54 +0200

    refactor/add debug window object (#18)
    * Add Debugger object for window
    
    * Change dependencies in Cargo.toml

--- crates/windows/src/debugger.rs
+++ crates/windows/src/debugger.rs
@@ -4,9 +4,67 @@ use iced_glutin::glutin::{
     event::WindowEvent,
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder, WindowId},
-    ContextBuilder, ContextCurrentState, ContextWrapper, PossiblyCurrent,
+    ContextBuilder, ContextWrapper, PossiblyCurrent,
 };
+
 use iced_glutin::mouse;
+
+pub struct Debugger {
+    pub id: WindowId,
+    pub context: ContextWrapper<PossiblyCurrent, Window>,
+    pub state: debugger::State,
+    pub gl: glow::Context,
+}
+
+impl Debugger {
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let title = "Debugger";
+        let window_builder = WindowBuilder::new().with_title(title);
+        let windowed_context = ContextBuilder::new()
+            .build_windowed(window_builder, event_loop)
+            .unwrap();
+
+        let (windowed_context, gl) = {
+            unsafe {
+                let windowed_context = windowed_context.make_current().unwrap();
+
+                let gl = glow::Context::from_loader_function(|s| {
+                    windowed_context.get_proc_address(s) as *const _
+                });
+
+                (windowed_context, gl)
+            }
+        };
+        let state = debugger::State::new(windowed_context.window(), &gl);
+
+        let id = windowed_context.window().id();
+        Self {
+            id,
+            context: windowed_context,
+            state,
+            gl,
+        }
+    }
+
+    pub fn process_event(&self, event: WindowEvent) -> ControlFlow {
+        match event {
+            WindowEvent::Resized(_physical_size) => ControlFlow::Wait,
+            WindowEvent::CloseRequested => ControlFlow::Wait,
+            _ => ControlFlow::Wait,
+        }
+    }
+    pub fn update(&mut self) {
+        self.state.update(self.context.window().scale_factor());
+    }
+
+    pub fn redraw(&mut self) {
+        let mouse = self.state.draw(&self.gl);
+
+        self.context
+            .window()
+            .set_cursor_icon(mouse_interaction(mouse));
+    }
+}
 
 pub fn mouse_interaction(interaction: mouse::Interaction) -> CursorIcon {
     use mouse::Interaction;
@@ -22,52 +80,4 @@ pub fn mouse_interaction(interaction: mouse::Interaction) -> CursorIcon {
         Interaction::ResizingHorizontally => CursorIcon::EwResize,
         Interaction::ResizingVertically => CursorIcon::NsResize,
     }
-}
-
-pub fn generate_debugger(
-    event_loop: &EventLoop<()>,
-) -> (
-    WindowId,
-    ContextWrapper<PossiblyCurrent, Window>,
-    debugger::State,
-    glow::Context,
-) {
-    let title = "Debugger";
-    let window_builder = WindowBuilder::new().with_title(title);
-    let windowed_context = ContextBuilder::new()
-        .build_windowed(window_builder, event_loop)
-        .unwrap();
-
-    let (windowed_context, gl) = {
-        unsafe {
-            let windowed_context = windowed_context.make_current().unwrap();
-
-            let gl = glow::Context::from_loader_function(|s| {
-                windowed_context.get_proc_address(s) as *const _
-            });
-
-            (windowed_context, gl)
-        }
-    };
-    let debugger = debugger::State::new(windowed_context.window(), &gl);
-
-    let window_id = windowed_context.window().id();
-    (window_id, windowed_context, debugger, gl)
-}
-
-pub fn process_event<T: ContextCurrentState>(
-    _context: &mut ContextWrapper<T, Window>,
-    event: WindowEvent,
-) -> ControlFlow {
-    match event {
-        WindowEvent::Resized(_physical_size) => ControlFlow::Wait,
-        WindowEvent::CloseRequested => ControlFlow::Wait,
-        _ => ControlFlow::Wait,
-    }
-}
-
-pub fn redraw(debugger: &mut debugger::State, gl: &glow::Context, window: &Window) {
-    let mouse = debugger.draw(gl);
-
-    window.set_cursor_icon(mouse_interaction(mouse));
 }
--- crates/windows/src/windows.rs
+++ crates/windows/src/windows.rs
@@ -8,32 +8,31 @@ pub struct Windows {}
 impl Windows {
     pub fn run() {
         let event_loop = EventLoop::new();
-        let (debugger_id, mut debugger_context, mut debugger, gl) =
-            debugger::generate_debugger(&event_loop);
+        let mut debugger = debugger::Debugger::new(&event_loop);
 
         let (emulator_id, mut emulator) = emulator::generate_emulator(&event_loop);
 
         event_loop.run(move |event, _, control_flow| match event {
             Event::LoopDestroyed => (),
-            Event::WindowEvent { event, window_id } if window_id == debugger_id => {
-                *control_flow = debugger::process_event(&mut debugger_context, event)
+            Event::WindowEvent { event, window_id } if window_id == debugger.id => {
+                *control_flow = debugger.process_event(event)
             }
             Event::WindowEvent { event, window_id } if window_id == emulator_id => {
                 *control_flow = emulator::process_event(&mut emulator, event)
             }
             Event::MainEventsCleared => {
                 // If there are events pending
-                if !debugger.state.is_queue_empty() {
+                if !debugger.state.state.is_queue_empty() {
                     // We update iced
-                    debugger.update(debugger_context.window().scale_factor());
+                    debugger.update();
 
                     // and request a redraw
-                    debugger_context.window().request_redraw();
+                    debugger.context.window().request_redraw();
                 }
             }
-            Event::RedrawRequested(window_id) if window_id == debugger_id => {
-                debugger::redraw(&mut debugger, &gl, debugger_context.window());
-                debugger_context.swap_buffers().unwrap();
+            Event::RedrawRequested(window_id) if window_id == debugger.id => {
+                debugger.redraw();
+                debugger.context.swap_buffers().unwrap();
             }
             Event::RedrawRequested(window_id) if window_id == emulator_id => {
                 // Do emulator redraw
