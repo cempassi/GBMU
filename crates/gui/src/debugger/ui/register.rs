93e0ff78ffcba313eda24cf63bf0ce3882576264
Cedric M'Passi <cempassi@student.42.fr>
2021-09-23 21:19:42 +0200

    [GUI] Use Container for registers

--- crates/gui/src/debugger/ui/register.rs
+++ crates/gui/src/debugger/ui/register.rs
@@ -1,34 +1,23 @@
-use iced_wgpu::{Checkbox, Renderer, Row, Text};
+use crate::fonts;
+use crate::theme::Theme;
+use iced_wgpu::{Container, Renderer, Row, Text};
 use iced_winit::Element;
 
 #[derive(Default)]
 pub struct Registers {
-    theme: crate::theme::Theme,
+    theme: Theme,
     is_checked: bool,
 }
 
 #[derive(Debug, Clone)]
-pub enum Message {
-    CheckboxToogled(bool),
-}
+pub enum Message {}
 
 impl Registers {
-    pub fn update(&mut self, message: Message) {
-        println!("Update of Registers reached");
-        match message {
-            Message::CheckboxToogled(is_checked) => self.is_checked = is_checked,
-        }
-        println!("I'm in the update function of the register");
-    }
+    pub fn update(&mut self, message: Message) {}
 
     pub fn view(&mut self) -> Element<Message, Renderer> {
-        let checkbox = Checkbox::new(self.is_checked, "Merge", |is_checked| {
-            Message::CheckboxToogled(is_checked)
-        })
-        .style(self.theme);
-
-        let hello_world =
-            Text::new("Hello, world! Are we doing this or what?").color([0.0, 0.0, 1.0]);
-        Row::new().push(checkbox).push(hello_world).into()
+        let number = format!("{:#x}", 256);
+        let text = Text::new(number).font(fonts::HASKLIG_LIGHT);
+        Container::new(text).style(self.theme).padding(5).into()
     }
 }
