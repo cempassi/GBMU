7686b271b27cde4ee3a85d2e97e697e560646918
Cedric M'Passi <cempassi@student.42.fr>
2021-09-26 22:17:03 +0200

    Change path for fonts

--- crates/gui/src/fonts.rs
+++ crates/gui/src/fonts.rs
@@ -2,10 +2,10 @@ use iced_winit::Font;
 
 pub const HASKLIG_BOLD: Font = Font::External {
     name: "Hasklig Bold",
-    bytes: include_bytes!("../../../ressources/fonts/Hasklig/Hasklig-Bold.ttf"),
+    bytes: include_bytes!("../../../ressources/fonts/hasklig/Hasklig-Bold.ttf"),
 };
 
 pub const HASKLIG_LIGHT: Font = Font::External {
     name: "Hasklig light",
-    bytes: include_bytes!("../../../ressources/fonts/Hasklig/Hasklig-Light.ttf"),
+    bytes: include_bytes!("../../../ressources/fonts/hasklig/Hasklig-Light.ttf"),
 };
