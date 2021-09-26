baec6bc26872507155e5603914fd46e9981e1eea
Cedric M'Passi <cempassi@student.42.fr>
2021-09-26 17:28:41 +0200

    Add lifetime to SOC in debugger gui

--- crates/gui/src/debugger/ui.rs
+++ crates/gui/src/debugger/ui.rs
@@ -1,8 +1,29 @@
 mod register;
 
-use iced_glow::Renderer;
-use iced_glutin::Clipboard;
-use iced_native::{Column, Command, Element, Program, Text};
+use crate::style::Theme;
+
+use iced_wgpu::{Column, Renderer};
+use iced_winit::{Command, Element, Program};
+use self::memory::{Memory, MemoryMsg};
+use registers::{CpuMsg, CpuRegisters};
+use soc::SOC;
+use std::convert::From;
+
+pub struct UserInterface {
+    theme: Theme,
+    cpu_registers: CpuRegisters,
+    memory: Memory,
+}
+
+impl<'a> From<&SOC<'a>> for UserInterface {
+    fn from(soc: &SOC) -> UserInterface {
+        Self {
+            theme: Theme::default(),
+            cpu_registers: CpuRegisters::new(soc.get_cpu_registers()),
+            memory: Memory::new(),
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Message {
