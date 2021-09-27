c94de5abcd3f373d07b4398c2e2ea3d397f08967
Cedric M'Passi <cempassi@student.42.fr>
2021-09-27 02:56:00 +0200

    Flaten cpu module

--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -1,5 +1,12 @@
 pub(crate) mod bus;
 pub mod cpu;
+pub mod area;
+mod flags;
+mod opcodes;
+mod pc;
+mod registers;
+use registers::New;
+
 
 pub use crate::bus::RegisterBus;
 pub use crate::cpu::area;
