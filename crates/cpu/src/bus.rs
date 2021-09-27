960a447ef4f81a3afce9373970c5bb6e43030a3b
Cedric M'Passi <cempassi@student.42.fr>
2021-09-27 02:25:18 +0200

    Add RegisterBus to cpu module

---
+++ crates/cpu/src/bus.rs
@@ -0,0 +1,7 @@
+use std::fmt::Debug;
+
+pub trait RegisterBus<T, O> : Debug  {
+    fn get(&self, _: T) -> O;
+
+    fn set(&mut self, _: T, data: O);
+}
--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod cpu;
+pub(crate) mod bus;
 
 pub use crate::cpu::area;
 pub use crate::cpu::Cpu;
 pub use crate::cpu::Registers;
+pub use crate::bus::RegisterBus;
