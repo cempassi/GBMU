c5c0c44ecba4f2b9709c34d327be9a9c06c1389e
guvillat <villat.guillaume@gmail.com>
2021-09-28 04:56:58 +0200

    Format for PR

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,7 +1,7 @@
-mod load_rr_b16;
 mod load_r_b8;
 mod load_r_r;
+mod load_rr_b16;
 
-pub use load_rr_b16::LoadRR16b;
 pub use load_r_b8::LoadR8b;
 pub use load_r_r::LoadR1R2;
+pub use load_rr_b16::LoadRR16b;
