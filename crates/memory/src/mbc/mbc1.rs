6080de540ad9b72f4845e534a8621769158939da
Cedric M'Passi <cempassi@student.42.fr>
2021-09-28 22:54:42 +0200

    Fix Mbc1 implementation

--- crates/memory/src/mbc/mbc1.rs
+++ crates/memory/src/mbc/mbc1.rs
@@ -36,7 +36,7 @@ impl MemoryBus for Mbc1 {
     }
 
     fn set(&mut self, address: usize, data: u8) {
-        <Self as MbcBus>::set(address, data);
+        <Self as MbcBus>::set(&mut self, address, data);
     }
 }
 
