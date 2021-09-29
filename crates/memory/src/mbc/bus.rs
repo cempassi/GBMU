5ded5b1e4dd8f1b62f532ebf0f174a281e615368
Cedric M'Passi <cempassi@student.42.fr>
2021-09-29 19:27:04 +0200

    Add AsRef as type constraint for Mbc trait

--- crates/memory/src/mbc/bus.rs
+++ crates/memory/src/mbc/bus.rs
@@ -1,8 +1,9 @@
 use crate::MemoryBus;
 use shared::Error;
+use std::convert::AsRef;
 
 pub trait MbcBus: std::fmt::Debug {
     fn set(&mut self, address: usize, data: u8) -> Result<(), Error>;
 }
 
-pub trait Mbc: MbcBus + MemoryBus {}
+pub trait Mbc: MbcBus + MemoryBus + AsRef<Vec<u8>> {}
