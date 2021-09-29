7916637f925d1eb2ed5a63d884c06d321b3895dd
Cedric M'Passi <cempassi@student.42.fr>
2021-09-29 19:38:21 +0200

    Move AsRef<Vec<u8>> type constrait to MemoryBus

--- crates/memory/src/bus.rs
+++ crates/memory/src/bus.rs
@@ -1,4 +1,6 @@
-pub trait MemoryBus: std::fmt::Debug {
+use std::convert::AsRef;
+
+pub trait MemoryBus: std::fmt::Debug + AsRef<Vec<u8>> {
     fn get(&self, _: usize) -> u8;
     fn set(&mut self, _: usize, data: u8);
 }
--- crates/memory/src/mbc/bus.rs
+++ crates/memory/src/mbc/bus.rs
@@ -1,9 +1,8 @@
 use crate::MemoryBus;
 use shared::Error;
-use std::convert::AsRef;
 
 pub trait MbcBus: std::fmt::Debug {
     fn set(&mut self, address: usize, data: u8) -> Result<(), Error>;
 }
 
-pub trait Mbc: MbcBus + MemoryBus + AsRef<Vec<u8>> {}
+pub trait Mbc: MbcBus + MemoryBus {}
