02f8842149c911ea91de778e95019e9e40e0c892
Villat Guillaume <villat.guillaume@gmail.com>
2021-09-30 05:16:27 +0200

    feature: Opcode load data from (HL) into Register N (#52)
    * Add Opcode Load Reg HL
    
    * Idk why test_load_reg_l_hl and test_load_reg_h_hl fail
    
    * Modif Error in test load reg H HL et L HL
    jsuis con je check un valeur a une addresse differente vu que je modif H ou L
    
    * Modif for PR

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,9 +1,11 @@
 mod load_hl_8b;
 mod load_r_b8;
 mod load_r_r;
+mod load_reg_hl;
 mod load_rr_b16;
 
 pub use load_hl_8b::LoadHL8b;
 pub use load_r_b8::LoadR8b;
 pub use load_r_r::LoadR1R2;
+pub use load_reg_hl::LoadRegHL;
 pub use load_rr_b16::LoadRR16b;
