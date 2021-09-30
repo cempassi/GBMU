4040d5c242b0d75f4c7d8bc8b4084843123e5a9b
Cedric M'Passi <cempassi@student.42.fr>
2021-09-30 03:46:59 +0200

    Impl From Memory for Memory UI

--- crates/ui/src/debugger/memory_map.rs
+++ crates/ui/src/debugger/memory_map.rs
@@ -1,35 +1,38 @@
-use super::widgets::hexdump;
-use crate::style::Theme;
 use iced_wgpu::{scrollable, Renderer, Scrollable};
 use iced_winit::Element;
+use std::convert::From;
 
-const LOREM_IPSUM: &[u8] = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed \
-                             interdum massa interdum gravida gravida. Nam ullamcorper.";
+use memory::Memory as MemoryData;
+use memory::Area;
+use super::widgets::hexdump;
+use crate::style::Theme;
 
 pub struct Memory {
-    hexdump: hexdump::State,
+    state: hexdump::State,
     scrollable: scrollable::State,
 }
 
 #[derive(Debug, Clone)]
 pub enum MemoryMsg {}
 
-impl Memory {
-    pub fn new() -> Self {
-        let mut hexdump = hexdump::State::default();
-        hexdump.load(LOREM_IPSUM);
+impl From<MemoryData> for Memory {
+    fn from(memory: MemoryData) -> Self {
+        let data = memory.borrow().get_area(Area::Bios);
+        let state = hexdump::State::new(data);
         let scrollable = scrollable::State::new();
-
         Self {
-            hexdump,
+            state,
             scrollable,
         }
     }
+}
 
+
+impl Memory {
     pub fn update(&self, _message: MemoryMsg) {}
 
     pub fn view(&mut self, _theme: Theme) -> Element<MemoryMsg, Renderer> {
-        let hexdump = hexdump::Hexdump::new(&mut self.hexdump);
+        let hexdump = hexdump::Hexdump::new(&mut self.state);
         Scrollable::new(&mut self.scrollable).push(hexdump).into()
     }
 }
