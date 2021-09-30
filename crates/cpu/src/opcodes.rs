8008aa9efa1c97bbdfb8a1333729b7fd6102822c
guvillat <villat.guillaume@gmail.com>
2021-09-30 08:51:06 +0200

    format for PR

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,17 +1,17 @@
 mod add_reg_a;
 mod data;
 mod load_hl_8b;
+mod load_hl_minus_a;
 mod load_r_b8;
 mod load_r_r;
 mod load_reg_hl;
 mod load_rr_b16;
-mod load_hl_minus_a;
 mod sub_reg_a;
 
 pub use add_reg_a::AddRegA;
 pub use data::Data;
-pub use load_hl_minus_a::LoadHLMRegA;
 pub use load_hl_8b::LoadHL8b;
+pub use load_hl_minus_a::LoadHLMRegA;
 pub use load_r_b8::LoadR8b;
 pub use load_r_r::LoadR1R2;
 pub use load_reg_hl::LoadRegHL;
