cc57ca5c0861dc27d880886b2cd3441cfb6371ff
Villat Guillaume <villat.guillaume@gmail.com>
2021-10-05 02:19:06 +0200

    Feature: Opcode add byte into Register A (#74)
    * Feature: OpCode ADC byte into Register A
    
    * Modif Doc
    
    * ADD enum Data for carry Operations
    
    * Rename Enum + file AddRegA
    Refacto
    
    * Stash mistake
    
    * Refacto Output of trait Add into u16 or u16, Flags
    
    * Rename + refacto
    
    * Expose Data + Refacto into Directories
    
    * Rename + refacto

---
+++ crates/cpu/src/opcodes/add_reg_a.rs
@@ -0,0 +1,159 @@
+use super::super::area::Bits8;
+use crate::area::Bits16;
+use crate::bus::RegisterBus;
+use crate::cpu::Registers;
+use crate::nextpc::NextPc;
+use crate::opcodes::data::arithmetic::Add;
+use crate::opcodes::data::Data;
+use memory::{Async, Memory};
+use num_enum::TryFromPrimitive;
+
+/// [ADD | ADC] A,n
+/// Description:
+///  Add [n | n + Carry flag] to A.
+/// Use with:
+///  n = A,B,C,D,E,H,L,(HL),8b
+/// Flags affected:
+///  Z - Set if result is zero.
+///  N - Reset.
+///  H - Set if carry from bit 3.
+///  C - Set if carry from bit 7.
+/// Opcodes:
+/// Instruction Parameters Opcode Cycles | Instruction Parameters Opcode Cycles
+/// ADD         A,A        0x87   4        ADD         A,E        0x83   4
+/// ADD         A,B        0x80   4        ADD         A,H        0x84   4
+/// ADD         A,C        0x81   4        ADD         A,L        0x85   4
+/// ADD         A,D        0x82   4        ADD         A,(HL)     0x86   8
+/// ADD         A,8b       0xc6   8
+/// ADC         A,A        0x8f   4        ADC         A,E        0x8b   4
+/// ADC         A,B        0x88   4        ADC         A,H        0x8c   4
+/// ADC         A,C        0x89   4        ADC         A,L        0x8d   4
+/// ADC         A,D        0x8a   4        ADC         A,(HL)     0x8e   8
+/// ADC         A,8b       0xce   8
+#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u8)]
+#[allow(clippy::upper_case_acronyms)]
+pub enum AddRegA {
+    AA = 0x8f,
+    AB = 0x88,
+    AC = 0x89,
+    AD = 0x8a,
+    AE = 0x8b,
+    AH = 0x8c,
+    AL = 0x8d,
+    AHL = 0x8e,
+    A8b = 0xce,
+    AcA = 0x87,
+    AcB = 0x80,
+    AcC = 0x81,
+    AcD = 0x82,
+    AcE = 0x83,
+    AcH = 0x84,
+    AcL = 0x85,
+    AcHL = 0x86,
+    Ac8b = 0xc6,
+}
+
+impl AddRegA {
+    pub async fn exec(self, registers: Registers, memory: Memory) {
+        let data: Data<u8> = match self {
+            AddRegA::AA => Data::NoCarry(registers.borrow().get(Bits8::A)),
+            AddRegA::AB => Data::NoCarry(registers.borrow().get(Bits8::B)),
+            AddRegA::AC => Data::NoCarry(registers.borrow().get(Bits8::C)),
+            AddRegA::AD => Data::NoCarry(registers.borrow().get(Bits8::D)),
+            AddRegA::AE => Data::NoCarry(registers.borrow().get(Bits8::E)),
+            AddRegA::AH => Data::NoCarry(registers.borrow().get(Bits8::H)),
+            AddRegA::AL => Data::NoCarry(registers.borrow().get(Bits8::L)),
+            AddRegA::AcA => Data::Carry(registers.borrow().get(Bits8::A)),
+            AddRegA::AcB => Data::Carry(registers.borrow().get(Bits8::B)),
+            AddRegA::AcC => Data::Carry(registers.borrow().get(Bits8::C)),
+            AddRegA::AcD => Data::Carry(registers.borrow().get(Bits8::D)),
+            AddRegA::AcE => Data::Carry(registers.borrow().get(Bits8::E)),
+            AddRegA::AcH => Data::Carry(registers.borrow().get(Bits8::H)),
+            AddRegA::AcL => Data::Carry(registers.borrow().get(Bits8::L)),
+            AddRegA::Ac8b => Data::Carry(registers.clone().next_pc(memory.clone()).await.unwrap()),
+            AddRegA::A8b => Data::NoCarry(registers.clone().next_pc(memory.clone()).await.unwrap()),
+            AddRegA::AHL => {
+                let src = registers.borrow().get(Bits16::HL);
+                Data::NoCarry(<Memory as Async>::get(memory, src).await.unwrap())
+            }
+            AddRegA::AcHL => {
+                let src = registers.borrow().get(Bits16::HL);
+                Data::Carry(<Memory as Async>::get(memory, src).await.unwrap())
+            }
+        };
+        let data = data.add(registers.borrow().get(Bits8::A));
+        registers.borrow_mut().set(Bits16::AF, data);
+    }
+}
+
+#[cfg(test)]
+mod test_instruction_add_reg_a_8b {
+    use super::AddRegA;
+    use crate::area::{Bits16, Bits8, Flag};
+    use crate::{executor, RegisterBus, Registers};
+    use memory::Memory;
+
+    #[test]
+    fn test_load_add_reg_a_8b() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::A8b;
+        register.borrow_mut().set(Bits8::A, 0x4f);
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0x80);
+        assert_eq!(register.borrow().get(Flag::H), true);
+    }
+
+    #[test]
+    fn test_load_add_reg_a_hl() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::AHL;
+        register.borrow_mut().set(Bits8::A, 0xf8);
+        register.borrow_mut().set(Bits16::HL, 0xc008);
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0xf8);
+    }
+
+    #[test]
+    fn test_load_add_reg_a_reg_b() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::AB;
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0x00);
+        assert_eq!(register.borrow().get(Flag::Z), true);
+    }
+
+    #[test]
+    fn test_load_adc_reg_a_8b() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::Ac8b;
+        register.borrow_mut().set(Bits8::A, 0x4f);
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0x81);
+        assert_eq!(register.borrow().get(Flag::H), true);
+    }
+
+    #[test]
+    fn test_load_adc_reg_a_hl() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::AcHL;
+        register.borrow_mut().set(Bits8::A, 0xf8);
+        register.borrow_mut().set(Bits16::HL, 0xc008);
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0xf9);
+    }
+
+    #[test]
+    fn test_load_adc_reg_a_reg_b() {
+        let register = Registers::default();
+        let memory = Memory::default();
+        let instruction = AddRegA::AcB;
+        executor::execute(Box::pin(instruction.exec(register.clone(), memory.clone())));
+        assert_eq!(register.borrow().get(Bits8::A), 0x01);
+    }
+}
---
+++ crates/cpu/src/opcodes/data/arithmetic.rs
@@ -0,0 +1,159 @@
+use crate::opcodes::data::Data;
+use crate::Flags;
+
+fn carry(shift: usize, nbr1: usize, nbr2: usize, c: usize) -> bool {
+    let c = c as usize;
+    let max = (1 << shift) - 1;
+    (nbr1 & max) + (nbr2 & max) + (c & max) > max
+}
+
+pub trait Add<T> {
+    type Output;
+    fn add(&self, data: T) -> Self::Output;
+}
+
+impl Add<u8> for Data<u8> {
+    type Output = u16;
+    fn add(&self, nbr: u8) -> Self::Output {
+        let (value, c) = match self {
+            Data::Carry(value) => (*value as usize, 1),
+            Data::NoCarry(value) => (*value as usize, 0),
+        };
+        let max: usize = (1 << 8) - 1;
+        let data = (value + nbr as usize + c as usize) & max;
+        let h = carry(4, value, nbr as usize, c as usize);
+        let c = carry(8, value, nbr as usize, c as usize);
+        let z = data == 0;
+        let mut flag = Flags::default();
+        flag.set_z(z);
+        flag.set_h(h);
+        flag.set_c(c);
+        (data as u16) << 8 | Flags::into_bytes(flag)[0] as u16
+    }
+}
+
+impl Add<u16> for Data<u16> {
+    type Output = (u16, Flags);
+    fn add(&self, nbr: u16) -> Self::Output {
+        let (value, c) = match self {
+            Data::Carry(value) => (*value as usize, 1),
+            Data::NoCarry(value) => (*value as usize, 0),
+        };
+        let max: usize = (1 << 16) - 1;
+        let data = (value + nbr as usize + c) & max;
+        let h = carry(12, value, nbr as usize, c as usize);
+        let c = carry(16, value, nbr as usize, c as usize);
+        let z = data == 0;
+        let mut flag = Flags::default();
+        flag.set_z(z);
+        flag.set_h(h);
+        flag.set_c(c);
+        (data as u16, flag)
+    }
+}
+
+#[cfg(test)]
+mod test_arithmetics_functions {
+    use crate::opcodes::data::arithmetic::Add;
+    use crate::opcodes::data::Data;
+    use crate::Flags;
+
+    #[test]
+    fn test_add_8b() {
+        let data: Data<u8> = Data::NoCarry(0x12);
+        assert_eq!(data.add(0x22), 0x3400);
+    }
+
+    #[test]
+    fn test_add_with_carry_8b() {
+        let data: Data<u8> = Data::Carry(0x12);
+        assert_eq!(data.add(0x22), 0x3500);
+    }
+
+    #[test]
+    fn test_add_8b_h_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x12);
+        flag.set_h(true);
+        assert_eq!(data.add(0x2f), 0x4140);
+    }
+
+    #[test]
+    fn test_add_8b_c_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x12);
+        flag.set_c(true);
+        assert_eq!(data.add(0xf0), 0x0280);
+    }
+
+    #[test]
+    fn test_add_with_carry_8b_c_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::Carry(0x12);
+        flag.set_c(true);
+        assert_eq!(data.add(0xf0), 0x0380);
+    }
+
+    #[test]
+    fn test_add_with_carry_8b_h_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::Carry(0x12);
+        flag.set_h(true);
+        assert_eq!(data.add(0x2f), 0x4240);
+    }
+
+    #[test]
+    fn test_add_8b_h_c_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x0a);
+        flag.set_h(true);
+        flag.set_c(true);
+        assert_eq!(data.add(0xfa), 0x04c0);
+    }
+
+    #[test]
+    fn test_add_with_carry_8b_h_c_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::Carry(0x0a);
+        flag.set_h(true);
+        flag.set_c(true);
+        assert_eq!(data.add(0xfa), 0x05c0);
+    }
+
+    #[test]
+    fn test_add_8b_z_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x00);
+        flag.set_z(true);
+        assert_eq!(data.add(0x00), 0x0010);
+    }
+
+    #[test]
+    fn test_add_8b_z_c_flag() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x20);
+        flag.set_c(true);
+        flag.set_z(true);
+        assert_eq!(data.add(0xe0), 0x0090);
+    }
+
+    #[test]
+    fn test_add_8b_all_flags() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::NoCarry(0x08);
+        flag.set_z(true);
+        flag.set_c(true);
+        flag.set_h(true);
+        assert_eq!(data.add(0xf8), 0x00d0);
+    }
+
+    #[test]
+    fn test_add_with_carry_8b_all_flags() {
+        let mut flag = Flags::default();
+        let data: Data<u8> = Data::Carry(0x07);
+        flag.set_z(true);
+        flag.set_c(true);
+        flag.set_h(true);
+        assert_eq!(data.add(0xf8), 0x00d0);
+    }
+}
