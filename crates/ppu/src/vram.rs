abd2e8146266e57ba9143ecd55dfb37873753a1b
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-05 04:01:45 +0200

    Add Vram

---
+++ crates/ppu/src/vram.rs
@@ -0,0 +1,55 @@
+const VRAM_SIZE: usize = 8192;
+
+pub struct Vram {
+    data: Vec<u8>,
+}
+
+impl Default for Vram {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl AsRef<Vec<u8>> for Vram {
+    fn as_ref(&self) -> &Vec<u8> {
+        self.data.as_ref()
+    }
+}
+
+impl Vram {
+    pub fn new() -> Self {
+        Self {
+            data: vec![0; VRAM_SIZE],
+        }
+    }
+
+    pub fn get(&self, address: usize) -> u8 {
+        self.data[address]
+    }
+
+    pub fn set(&mut self, address: usize, data: u8) {
+        self.data[address] = data;
+    }
+}
+
+#[cfg(test)]
+mod test_wram {
+    use super::Vram;
+
+    #[test]
+    fn test_read_vram() {
+        let vram = Vram::default();
+
+        assert_eq!(vram.get(0x10), 0);
+    }
+
+    #[test]
+    fn test_write_read_vram() {
+        let mut vram = Vram::default();
+
+        vram.set(0x42, 42);
+        let read = vram.get(0x42);
+
+        assert_eq!(read, 42);
+    }
+}
