77e6dd7fbfff7a47a84a55cdac940fc199af516b
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-06 04:23:25 +0200

    Add ppu dependency to Memory crate

--- crates/ppu/src/interface.rs
+++
@@ -1,7 +0,0 @@
-use crate::registers;
-use crate::vram;
-use std::cell::RefCell;
-use std::rc::Rc;
-
-pub type Vram = Rc<RefCell<vram::Vram>>;
-pub type Registers = Rc<RefCell<registers::Registers>>;
