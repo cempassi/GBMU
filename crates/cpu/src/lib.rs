7e538500d789a6a7811435f329444e2181cae1dd
Cedric M'Passi <cempassi@student.42.fr>
2021-10-14 01:00:31 +0200

    feature: Get data at register address (#100)
    * Implement GetAt for Registers
    
    * Format code

--- crates/cpu/src/lib.rs
+++ crates/cpu/src/lib.rs
@@ -3,12 +3,15 @@ pub mod bus;
 pub mod cpu;
 pub(crate) mod executor;
 mod flags;
+pub mod get_at;
 pub mod interface;
 pub mod nextpc;
 mod opcodes;
+pub mod reader;
 pub mod registers;
 
 pub use crate::bus::RegisterBus;
 pub use crate::cpu::Cpu;
 pub use crate::interface::{NewRegisters, Registers};
+pub(crate) use crate::reader::Reader;
 pub(crate) use flags::Flags;
