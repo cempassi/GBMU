2b078319a97e535752d7e0b13c7145e13aec2417
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-14 17:21:17 +0200

    Move LoadRegister into bits8 module

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,17 +1,17 @@
 mod add_reg_a;
+mod bits8;
 mod cb_operation;
 mod consts;
 mod data;
 mod load_hl_8b;
-mod load_register;
 mod load_rr_b16;
 mod rotate;
 mod sub_reg_a;
 
 pub use add_reg_a::AddRegA;
+pub use bits8::LoadRegister;
 pub use data::Data;
 pub use load_hl_8b::LoadHL8b;
-pub use load_register::LoadRegister;
 pub use load_rr_b16::LoadRR16b;
 pub use rotate::Rotate;
 pub use sub_reg_a::SubRegA;
---
+++ crates/cpu/src/opcodes/bits8.rs
@@ -0,0 +1,3 @@
+mod load_register;
+
+pub use load_register::LoadRegister;
