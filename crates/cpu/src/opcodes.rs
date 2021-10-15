d64bb9726cd3165ade8aac6bc8393cc8944629d6
Cedric M'Passi <cempassi@student.42.fr>
2021-10-15 08:30:00 +0200

    refactor: Merge substraction and addition into one  (#106)

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,15 +1,13 @@
-mod add_reg_a;
+mod arithmetic;
 mod bits8;
 mod cb_operation;
 mod consts;
 mod data;
 mod load_rr_b16;
 mod rotate;
-mod sub_reg_a;
 
-pub use add_reg_a::AddRegA;
+pub use arithmetic::Arithmetic;
 pub use bits8::Load;
 pub use data::Data;
 pub use load_rr_b16::LoadRR16b;
 pub use rotate::Rotate;
-pub use sub_reg_a::SubRegA;
