5dcdff7e4821190ac9a555291148a23d140b6ed7
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-16 01:47:39 +0200

    Add SetAt and LoadData futures

--- crates/cpu/src/futures.rs
+++ crates/cpu/src/futures.rs
@@ -1,7 +1,9 @@
 mod getat;
 mod setat;
 mod stack;
+mod load_data;
 
 pub(crate) use getat::GetAt;
-pub(crate) use setat::SetAt;
+pub(crate) use setat::{SetAt, SetData};
+pub(crate) use load_data::LoadData;
 pub(crate) use stack::{Pop, Push};
