153bd973a31904ec449fd74209ab488fa8bc76a9
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-16 01:48:23 +0200

    Replace Stack by Load16b

--- crates/cpu/src/cpu.rs
+++ crates/cpu/src/cpu.rs
@@ -1,9 +1,8 @@
 pub use crate::interface::{NewRegisters, Registers};
 use crate::opcodes::Arithmetic;
 use crate::opcodes::Load;
-use crate::opcodes::LoadRR16b;
+use crate::opcodes::Load16b;
 use crate::opcodes::Rotate;
-use crate::opcodes::Stack;
 
 use crate::nextpc::NextPc;
 use memory::Memory;
@@ -60,9 +59,7 @@ impl Cpu {
             self.prefix_cb().await;
         } else if let Ok(operation) = Load::try_from_primitive(opcode) {
             operation.exec(self.registers, self.memory).await;
-        } else if let Ok(operation) = Stack::try_from_primitive(opcode) {
-            operation.exec(self.registers, self.memory).await;
-        } else if let Ok(operation) = LoadRR16b::try_from_primitive(opcode.into()) {
+        } else if let Ok(operation) = Load16b::try_from_primitive(opcode.into()) {
             operation.exec(self.registers, self.memory).await;
         } else if let Ok(operation) = Arithmetic::try_from_primitive(opcode) {
             operation.exec(self.registers, self.memory).await;
--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -3,13 +3,11 @@ mod bits8;
 mod cb_operation;
 mod consts;
 mod data;
-mod load_rr_b16;
+mod load16b;
 mod rotate;
-mod stack;
 
 pub use arithmetic::Arithmetic;
 pub use bits8::Load;
 pub use data::Data;
-pub use load_rr_b16::LoadRR16b;
+pub use load16b::Load16b;
 pub use rotate::Rotate;
-pub use stack::Stack;
