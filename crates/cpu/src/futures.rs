86d78384fdfde26f2b919f7a1c122d57be7f2885
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-15 21:39:00 +0200

    Add Pop and Push futures for registers

--- crates/cpu/src/futures.rs
+++ crates/cpu/src/futures.rs
@@ -1,41 +1,7 @@
-use super::area::Bits16;
-use crate::{Reader, RegisterBus, Registers};
-use memory::{Async, Memory};
-use shared::Error;
-use std::future::Future;
-use std::pin::Pin;
+mod getat;
+mod setat;
+mod stack;
 
-type Getter = Pin<Box<dyn Future<Output = Result<u8, Error>>>>;
-type Setter = Pin<Box<dyn Future<Output = Result<(), Error>>>>;
-
-pub(crate) trait GetAt {
-    fn get_at(self, memory: Memory, area: Bits16) -> Getter;
-}
-
-impl GetAt for Registers {
-    fn get_at(self, memory: Memory, area: Bits16) -> Getter {
-        let inner = Box::pin(get_at(self, memory, area));
-        Box::pin(Reader::new(inner))
-    }
-}
-
-async fn get_at(registers: Registers, memory: Memory, area: Bits16) -> Result<u8, Error> {
-    let address = registers.borrow().get(area);
-    memory.get::<u8>(address).await
-}
-
-pub(crate) trait SetAt {
-    fn set_at(self, memory: Memory, area: Bits16, data: u8) -> Setter;
-}
-
-impl SetAt for Registers {
-    fn set_at(self, memory: Memory, area: Bits16, data: u8) -> Setter {
-        let inner = Box::pin(set_at(self, memory, area, data));
-        Box::pin(Reader::new(inner))
-    }
-}
-
-async fn set_at(registers: Registers, memory: Memory, area: Bits16, data: u8) -> Result<(), Error> {
-    let address = registers.borrow().get(area);
-    memory.set::<u8>(address, data).await
-}
+pub(crate) use getat::GetAt;
+pub(crate) use setat::SetAt;
+pub(crate) use stack::{Pop, Push};
---
+++ crates/cpu/src/futures/getat.rs
@@ -0,0 +1,29 @@
+use crate::area::Bits16;
+use crate::registers::Arithmetic;
+use crate::{Reader, RegisterBus, Registers};
+use memory::{Async, Memory};
+use shared::Error;
+use std::future::Future;
+use std::pin::Pin;
+
+type Getter<T> = Pin<Box<dyn Future<Output = Result<T, Error>>>>;
+
+pub(crate) trait GetAt<T> {
+    fn get_at(self, memory: Memory, area: Bits16) -> Getter<T>;
+}
+
+impl GetAt<u8> for Registers {
+    fn get_at(self, memory: Memory, area: Bits16) -> Getter<u8> {
+        let address = self.borrow().get(area);
+        let inner = Box::pin(memory.get::<u8>(address));
+        Box::pin(Reader::new(inner))
+    }
+}
+
+impl GetAt<u16> for Registers {
+    fn get_at(self, memory: Memory, area: Bits16) -> Getter<u16> {
+        let address = self.borrow().get(area);
+        let inner = Box::pin(memory.get::<u16>(address));
+        Box::pin(Reader::new(inner))
+    }
+}
