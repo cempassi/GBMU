e53208f939787d47144ed60424c65eac902cace7
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-16 23:36:09 +0200

    Remove Data enum

--- crates/cpu/src/opcodes.rs
+++ crates/cpu/src/opcodes.rs
@@ -1,14 +1,12 @@
 mod arithmetic;
 mod cb_operation;
 mod consts;
-mod data;
 mod load;
 mod load16b;
 mod logical;
 mod rotate;
 
 pub use arithmetic::Arithmetic;
-pub use data::Data;
 pub use load::Load;
 pub use load16b::Load16b;
 pub use logical::Logic;
--- crates/cpu/src/opcodes/data.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod arithmetic;
-
-pub use arithmetic::Add;
-
-pub enum Data<T> {
-    Carry(T),
-    NoCarry(T),
-}
