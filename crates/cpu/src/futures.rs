d0c3629a0c9cf5d1906138f8017d3f98c997f169
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-16 04:15:23 +0200

    Add futures for calcul

--- crates/cpu/src/futures.rs
+++ crates/cpu/src/futures.rs
@@ -2,8 +2,10 @@ mod getat;
 mod load_data;
 mod setat;
 mod stack;
+mod calcul;
 
 pub(crate) use getat::GetAt;
 pub(crate) use load_data::LoadData;
 pub(crate) use setat::{SetAt, SetData};
 pub(crate) use stack::{Pop, Push};
+pub(crate) use calcul::{CalculHL, CalculNext, Operation};
