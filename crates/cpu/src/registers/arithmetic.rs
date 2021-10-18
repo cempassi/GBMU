14b46c5f060ae44151021915b881a4d546ace62d
Cedric M'Passi <cempassi@student.42.fr>
2021-10-18 03:17:36 +0200

    refactor: Change carry check in arithmetics (#114)

--- crates/cpu/src/registers/arithmetic.rs
+++ crates/cpu/src/registers/arithmetic.rs
@@ -31,9 +31,8 @@ impl Arithmetic<Bits8, u8> for Registers {
     fn add(&mut self, data: u8, use_carry: bool) {
         let carry: u8 = if use_carry && self.get(Flag::C) { 1 } else { 0 };
         let a = self.get(Bits8::A);
-        let result = a.wrapping_add(data).wrapping_add(carry);
+        let (result, check_carry) = a.overflowing_add(data + carry);
         let check_half = (a & HALF_U8) + (data & HALF_U8) + carry > HALF_U8;
-        let check_carry = (a as u16) + (data as u16) + carry as u16 > u8::MAX as u16;
         self.set(Bits8::A, result);
         self.set(Flag::H, check_half);
         self.set(Flag::C, check_carry);
@@ -43,9 +42,8 @@ impl Arithmetic<Bits8, u8> for Registers {
     fn sub(&mut self, data: u8, use_carry: bool) {
         let carry: u8 = if use_carry && self.get(Flag::C) { 1 } else { 0 };
         let a = self.get(Bits8::A);
-        let result = a.wrapping_sub(data).wrapping_sub(carry);
+        let (result, check_carry) = a.overflowing_sub(data + carry);
         let check_half = (a & HALF_U8) < (data & HALF_U8) + carry;
-        let check_carry = (a as u16) + (data as u16) + carry as u16 > u8::MAX as u16;
         self.set(Bits8::A, result);
         self.set(Flag::C, check_carry);
         self.set(Flag::H, check_half);
@@ -68,9 +66,8 @@ impl Arithmetic<Bits16, u16> for Registers {
     fn add(&mut self, data: u16, use_carry: bool) {
         let carry: u16 = if use_carry && self.get(Flag::C) { 1 } else { 0 };
         let hl = self.get(Bits16::HL);
-        let result = hl.wrapping_add(data).wrapping_add(carry);
+        let (result, check_carry) = hl.overflowing_add(data + carry);
         let check_half = (hl & HALF_U16) + (data & HALF_U16) + carry > HALF_U16;
-        let check_carry = (hl as u32) + (data as u32) + carry as u32 > u16::MAX as u32;
         self.set(Bits16::HL, result);
         self.set(Flag::H, check_half);
         self.set(Flag::C, check_carry);
