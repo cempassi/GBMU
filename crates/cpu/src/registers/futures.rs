31dc47d2ad677ff8a69cc8073ca2c05d5901104e
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-19 00:00:24 +0200

    Format code

--- crates/cpu/src/registers/futures.rs
+++ crates/cpu/src/registers/futures.rs
@@ -8,9 +8,9 @@ mod reader;
 mod set;
 mod setat;
 
-pub(crate) use r#async::Async;
 pub(crate) use calcul::Logical;
 pub(crate) use getat::GetAt;
 pub(crate) use jump::Jump;
 pub(crate) use nextpc::NextPc;
+pub(crate) use r#async::Async;
 pub(crate) use setat::SetAt;
