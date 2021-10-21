d419ec24e7c71ed3bdd4ab5345b770dfe615f66e
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-22 01:29:18 +0200

    Add Line button to UI

--- crates/ui/src/debugger/menu.rs
+++ crates/ui/src/debugger/menu.rs
@@ -1,42 +1,49 @@
-use crate::style::{fonts, Theme};
-use iced_wgpu::{button, Button, Renderer, Row, Text};
+mod tick;
+mod line;
+
+use crate::style::Theme;
+use iced_wgpu::{Renderer, Row};
 use iced_winit::{alignment::Alignment, Element, Length, Space};
 use soc::Runner;
+use line::Line;
+use tick::Tick;
 
 pub struct Menu {
-    runner: Runner,
-    next_tick: button::State,
+    line: Line,
+    tick: Tick
+
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum MenuMsg {
     TickPressed,
+    LinePressed,
 }
 
 impl Menu {
     pub fn new(runner: Runner) -> Self {
-        let next_tick = button::State::default();
-        Self { runner, next_tick }
+        let line = Line::new(runner.clone());
+        let tick = Tick::new(runner.clone());
+        Self { tick, line }
     }
     pub fn update(&mut self, message: MenuMsg) {
         match message {
-            MenuMsg::TickPressed => self.runner.borrow_mut().tick(),
+            MenuMsg::TickPressed => self.tick.update(),
+            MenuMsg::LinePressed => self.line.update()
         }
     }
 
     pub fn view(&mut self, theme: Theme) -> Element<MenuMsg, Renderer> {
-        let tick_text = Text::new("Tick").font(fonts::HASKLIG_LIGHT).size(20);
-        let next_tick = Button::new(&mut self.next_tick, tick_text)
-            .on_press(MenuMsg::TickPressed)
-            .padding(10)
-            .style(theme);
+        let line = self.line.view(theme);
+        let tick = self.tick.view(theme);
         let space = Space::new(Length::Units(35), Length::Units(0));
         Row::new()
             .align_items(Alignment::End)
             .width(Length::Fill)
             .padding(5)
+            .push(tick)
             .push(space)
-            .push(next_tick)
+            .push(line)
             .into()
     }
 }
