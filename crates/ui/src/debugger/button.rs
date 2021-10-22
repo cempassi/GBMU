f1563ad42b6187b49b9edf6afa4f3dde4ae1614f
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-23 01:31:38 +0200

    Add ui for Frame step

--- crates/ui/src/debugger/button.rs
+++ crates/ui/src/debugger/button.rs
@@ -1,38 +1,52 @@
 use crate::style::{fonts, Theme};
-use iced_wgpu::{button, Button, Renderer, Row, Text};
-use iced_winit::{alignment::Alignment, Element, Length, Space};
+use iced_wgpu::{button, Renderer, Text};
+use iced_winit::{Element, Length};
 
-use super::MenuMsg;
+use super::menu::MenuMsg;
 use soc::Runner;
 
-pub struct Line {
+pub struct Button {
     runner: Runner,
+    message: MenuMsg,
     state: button::State,
 }
 
-impl Line {
-    pub fn new(runner: Runner) -> Self {
+impl Button {
+    pub fn new(runner: Runner, message: MenuMsg) -> Self {
         let state = button::State::default();
-        Self { runner, state }
+        Self {
+            runner,
+            state,
+            message,
+        }
     }
 
-    pub fn update(&mut self) {
-        self.runner.borrow_mut().line();
+    pub fn update(&self) {
+        match self.message {
+            MenuMsg::Tick => {
+                self.runner.borrow_mut().tick();
+            }
+            MenuMsg::Line => {
+                self.runner.borrow_mut().line();
+            }
+            MenuMsg::Frame => {
+                self.runner.borrow_mut().frame();
+            }
+        }
+    }
+
+    pub fn is_button(&self, message: MenuMsg) -> bool {
+        self.message == message
     }
 
     pub fn view(&mut self, theme: Theme) -> Element<MenuMsg, Renderer> {
-        let tick_text = Text::new("Line").font(fonts::HASKLIG_LIGHT).size(20);
-        let next_tick = Button::new(&mut self.state, tick_text)
-            .on_press(MenuMsg::LinePressed)
-            .padding(10)
-            .style(theme);
-        let space = Space::new(Length::Units(35), Length::Units(0));
-        Row::new()
-            .align_items(Alignment::End)
+        let text = Text::new(format!("{:?}", self.message))
+            .font(fonts::HASKLIG_LIGHT)
+            .size(20);
+        button::Button::new(&mut self.state, text)
+            .on_press(self.message)
             .width(Length::Fill)
-            .padding(5)
-            .push(space)
-            .push(next_tick)
+            .style(theme)
             .into()
     }
 }
