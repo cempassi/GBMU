7ddfa391874c1c75a8221f00807fe407292c43de
Cedric M'PASSI <cempassi@student.42.fr>
2021-10-25 19:06:28 +0200

    Add Cpu and Ppu menu buttons

--- crates/ui/src/debugger/menu.rs
+++ crates/ui/src/debugger/menu.rs
@@ -1,11 +1,13 @@
 use super::button::Button;
 use crate::style::Theme;
-use iced_wgpu::{Renderer, Row};
-use iced_winit::{Alignment, Element, Length, Space};
+use iced_native::Length;
+use iced_wgpu::{Renderer, Row, Space};
+use iced_winit::Element;
 use soc::Runner;
 
 pub struct Menu {
-    buttons: Vec<Button>,
+    right: Vec<Button>,
+    left: Vec<Button>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -13,43 +15,53 @@ pub enum MenuMsg {
     Tick,
     Line,
     Frame,
+    Cpu,
+    Ppu,
 }
 
 impl Menu {
     pub fn new(runner: Runner) -> Self {
         let tick = Button::new(runner.clone(), MenuMsg::Tick);
         let line = Button::new(runner.clone(), MenuMsg::Line);
-        let frame = Button::new(runner, MenuMsg::Frame);
+        let frame = Button::new(runner.clone(), MenuMsg::Frame);
+        let cpu = Button::new(runner.clone(), MenuMsg::Cpu);
+        let ppu = Button::new(runner, MenuMsg::Ppu);
 
         Self {
-            buttons: vec![tick, line, frame],
+            left: vec![cpu, ppu],
+            right: vec![tick, line, frame],
         }
     }
 
     pub fn update(&mut self, message: MenuMsg) {
-        if let Some(button) = self
-            .buttons
-            .iter()
-            .find(|&button| button.is_button(message))
-        {
+        if let Some(button) = self.right.iter().find(|&button| button.is_button(message)) {
             button.update()
         };
     }
 
     pub fn view(&mut self, theme: Theme) -> Element<MenuMsg, Renderer> {
-        self.buttons
+        let right: Element<MenuMsg, Renderer> = self
+            .right
             .iter_mut()
-            .fold(
-                Row::new()
-                    .align_items(Alignment::End)
-                    .width(Length::Fill)
-                    .padding(5),
-                |row, button| {
-                    let space = Space::new(Length::Units(10), Length::Shrink);
-                    let element: Element<MenuMsg, Renderer> = button.view(theme);
-                    row.push(element).push(space)
-                },
-            )
+            .fold(Row::new().spacing(10), |row, button| {
+                let element: Element<MenuMsg, Renderer> = button.view(theme);
+                row.push(element)
+            })
+            .into();
+        let left: Element<MenuMsg, Renderer> = self
+            .left
+            .iter_mut()
+            .fold(Row::new().spacing(10), |row, button| {
+                let element: Element<MenuMsg, Renderer> = button.view(theme);
+                row.push(element)
+            })
+            .into();
+        let space = Space::with_width(Length::Fill);
+        Row::new()
+            .push(left)
+            .push(space)
+            .push(right)
+            .padding(5)
             .into()
     }
 }
