758c536a731e1979a6b41c01c4032b3eb8fde5f4
Cedric M'PASSI <cempassi@student.42.fr>
2021-11-08 01:14:30 +0100

    Add UI for Cpu Register flags

---
+++ crates/ui/src/debugger/cpu/flags.rs
@@ -0,0 +1,45 @@
+use super::{CpuMsg, View};
+use iced_graphics::Alignment;
+use itertools::Itertools;
+use cpu::registers::{self, Bus};
+use iced_native::Element;
+use iced_wgpu::{Column, Row, Renderer};
+
+use crate::{debugger::widgets::{Flag, Text}, style::Theme};
+
+pub struct Flags {
+    flags: Vec<cpu::registers::Flag>
+}
+
+impl Flags {
+    pub fn new(flags: Vec<cpu::registers::Flag>) -> Self {
+        Self { flags }
+    }
+
+    pub fn view(&self, registers: &cpu::Registers, theme: Theme) -> Element<CpuMsg, Renderer> {
+        let title = Text::new("Flags").medium_it(20);
+        let mut column = Column::new().push(title).align_items(Alignment::Center);
+        for (left, right) in self.flags.iter().tuples() {
+            let mut row = Row::new().spacing(15);
+            row = row.push(left.view(registers, theme));
+            row = row.push(right.view(registers, theme));
+            column = column.push(row);
+        }
+        column.into()
+    }
+}
+
+impl View<CpuMsg> for registers::Flag {
+    fn get_data(&self, registers: &cpu::Registers) -> String {
+        format!("{}", registers.get(*self))
+    }
+
+    fn get_name(&self) -> String {
+        format!("{:?}", *self)
+    }
+
+    fn view(&self, registers: &cpu::Registers, _theme: Theme) -> Element<CpuMsg, Renderer> {
+        let builder = Flag::new(20, 1, 5);
+        builder.render(self.get_name(), self.get_data(registers)).into()
+    }
+}
--- crates/ui/src/debugger/widgets.rs
+++ crates/ui/src/debugger/widgets.rs
@@ -2,7 +2,9 @@ pub(crate) mod cell;
 pub mod hexdump;
 pub(crate) mod register;
 pub(crate) mod text;
+pub(crate) mod flag;
 
 pub(crate) use cell::Cell;
 pub(crate) use register::Register;
 pub(crate) use text::Text;
+pub(crate) use flag::Flag;
