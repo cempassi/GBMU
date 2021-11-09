5fa2413a488ca6cef2b893e3706456ff95483ddd
Cedric M'PASSI <cempassi@student.42.fr>
2021-11-09 08:14:59 +0100

    Remove Hexdump

---
+++ crates/ui/src/debugger/ppu/coordinates.rs
@@ -0,0 +1,34 @@
+use super::PpuMsg;
+use super::View;
+use crate::debugger::widgets::Register;
+use crate::debugger::widgets::Text;
+use enum_iterator::IntoEnumIterator;
+use iced_graphics::Alignment;
+use iced_wgpu::{Column, Renderer, Row};
+use iced_winit::Element;
+use itertools::Itertools;
+use ppu::Coordinates;
+use ppu::Field;
+
+impl View<PpuMsg> for Coordinates {
+    fn view(&self, _theme: crate::style::Theme) -> Element<PpuMsg, Renderer> {
+        let title = Text::new("Lcd").medium_it(20);
+        let mut coordinates = Column::new().align_items(Alignment::Center).push(title);
+
+        for (left, right) in Field::into_enum_iter().tuples() {
+            let row = Row::new();
+
+            let name = format!("{}", left);
+            let data = self.get(left).to_string();
+            let row = row.push(Register::render(name, data));
+
+            let name = format!("{}", right);
+            let data = self.get(right).to_string();
+            let row = row.push(Register::render(name, data));
+
+            coordinates = coordinates.push(row)
+        }
+
+        coordinates.into()
+    }
+}
