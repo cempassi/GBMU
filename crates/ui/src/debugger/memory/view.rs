e6f72cc4210bdf8047304696cfb898fde333141e
Cedric M'PASSI <cempassi@student.42.fr>
2021-11-11 08:23:47 +0100

    Scrollable memory area improvement

--- crates/ui/src/debugger/memory/view.rs
+++ crates/ui/src/debugger/memory/view.rs
@@ -1,30 +1,25 @@
 use super::MemoryMsg;
 use crate::debugger::widgets::memory::Hexdump;
 use crate::style::Theme;
-use iced::{
-    alignment::{Horizontal, Vertical},
-    Column, Container, Length, Text,
-};
+use iced::{Column, Text};
 use memory::{Bus, Rom};
 use ppu::Ppu;
-
-const TAB_PADDING: u16 = 16;
 
 pub trait View {
     fn title(&self) -> Text;
 
-    fn view(&mut self, theme: Theme) -> Container<MemoryMsg> {
-        let column = Column::new()
+    fn view(&mut self, theme: Theme) -> Column<MemoryMsg> {
+        Column::new()
             .spacing(20)
             .push(self.title())
-            .push(self.content(theme));
+            .push(self.content(theme))
 
-        Container::new(column)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .align_x(Horizontal::Center)
-            .align_y(Vertical::Center)
-            .padding(TAB_PADDING)
+        // Container::new(column)
+        //     .width(Length::Fill)
+        //     .height(Length::Fill)
+        //     .align_x(Horizontal::Center)
+        //     .align_y(Vertical::Center)
+        //    .padding(TAB_PADDING)
     }
 
     fn content(&mut self, theme: Theme) -> Column<MemoryMsg>;
