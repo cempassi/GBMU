daacf8bd661dbfaf9392c8e17dfbdf8b63a5b81a
Cedric M'PASSI <cempassi@student.42.fr>
2021-11-15 20:05:37 +0100

    Change memory consts to Uppercase

--- crates/memory/src/consts.rs
+++ crates/memory/src/consts.rs
@@ -1,24 +1,24 @@
 /// Memory Areas
 pub const BIOS_MIN: u16 = 0x0000;
-pub const BIOS_MAX: u16 = 0x00ff;
+pub const BIOS_MAX: u16 = 0x00FF;
 pub const ROM_MIN: u16 = 0x000;
-pub const ROM_MAX: u16 = 0x7fff;
+pub const ROM_MAX: u16 = 0x7FFF;
 pub const VRAM_MIN: u16 = 0x8000;
-pub const VRAM_MAX: u16 = 0x9fff;
-pub const EXT_RAM_MIN: u16 = 0xa000;
-pub const EXT_RAM_MAX: u16 = 0xbfff;
-pub const WRAM_MIN: u16 = 0xc000;
-pub const WRAM_MAX: u16 = 0xdfff;
+pub const VRAM_MAX: u16 = 0x9FFF;
+pub const EXT_RAM_MIN: u16 = 0xA000;
+pub const EXT_RAM_MAX: u16 = 0xBFFF;
+pub const WRAM_MIN: u16 = 0xC000;
+pub const WRAM_MAX: u16 = 0xDFFF;
 pub const ECHO_MIN: u16 = 0xe000;
-pub const ECHO_MAX: u16 = 0xfdff;
-pub const OAM_MIN: u16 = 0xfe00;
-pub const OAM_MAX: u16 = 0xfe9f;
-pub const IOREG_MIN: u16 = 0xff00;
-pub const IOREM_MAX: u16 = 0xff7f;
-pub const HRAM_MIN: u16 = 0xff80;
-pub const HRAM_MAX: u16 = 0xfffe;
-pub const RESTRICTED_MIN: u16 = 0xfea0;
-pub const RESTRICTED_MAX: u16 = 0xfeff;
+pub const ECHO_MAX: u16 = 0xFDFF;
+pub const OAM_MIN: u16 = 0xFE00;
+pub const OAM_MAX: u16 = 0xFE9F;
+pub const IOREG_MIN: u16 = 0xFF00;
+pub const IOREM_MAX: u16 = 0xFF7F;
+pub const HRAM_MIN: u16 = 0xFF80;
+pub const HRAM_MAX: u16 = 0xFFFE;
+pub const RESTRICTED_MIN: u16 = 0xFEA0;
+pub const RESTRICTED_MAX: u16 = 0xFEFF;
 
 pub const HIGH_RAM_SIZE: usize = 127;
 
