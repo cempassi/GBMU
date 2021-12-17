fe2ab052ea39823912d0e5cf36702160498e2a51
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-17 03:55:02 +0100

    Add serial in memory

---
+++ crates/memory/src/serial.rs
@@ -0,0 +1,25 @@
+use crate::consts;
+
+#[derive(Debug, Default)]
+pub struct Serial {
+    data: u8,
+    control: u8
+}
+
+impl Serial {
+    pub fn get(&self, address: u16) -> u8 {
+        if address == consts::SERIAL_DATA {
+            self.data
+        } else {
+            self.control
+        }
+    }
+
+    pub fn set(&mut self, address: u16, data: u8) {
+        if address == consts::SERIAL_DATA {
+            self.data = data;
+        } else {
+            self.control = data;
+        }
+    }
+}
