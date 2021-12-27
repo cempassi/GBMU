80ab653ab7d589a6db948cb1ff3a06c4d988eefb
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-27 20:42:34 +0100

    Correct Shift opperations

--- crates/cpu/src/registers/shift.rs
+++ crates/cpu/src/registers/shift.rs
@@ -1,12 +1,6 @@
 use super::{Bits8, Flag};
 use super::{Bus, Registers};
 
-const BIT7: u8 = 0x80;
-const BIT0: u8 = 0x1;
-
-const LOWER_HALF: u8 = 0xF;
-const UPPER_HALF: u8 = 0xF0;
-
 /// Shift arithmetic and logic are right shifts.
 
 pub trait Shift<T> {
@@ -18,49 +12,44 @@ pub trait Shift<T> {
 
 impl Shift<Bits8> for Registers {
     fn shift_left(&mut self, dst: Bits8) -> u8 {
-        let mut data = self.get(dst);
-        let carry = (data & BIT7) != 0;
-        data <<= 1;
-        self.set(Flag::C, carry);
+        let data = self.get(dst);
+        let result = data << 1;
+        let bit7 = data >> 7;
+        self.set(Flag::C, bit7 == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        self.set(dst, data);
+        self.set(Flag::Z, result == 0);
+        self.set(dst, result);
         0
     }
 
     fn shift_arithmetic(&mut self, dst: Bits8) -> u8 {
-        let mut data = self.get(dst);
-        let carry = (data & BIT0) != 0;
-        let sign = data & BIT7;
-        data &= !(BIT7);
-        data >>= 1;
-        data |= sign;
-        self.set(Flag::C, carry);
+        let data = self.get(dst);
+        let result = data >> 1 | (data & 0x80);
+        self.set(Flag::C, (data & 1) == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        self.set(dst, data);
+        self.set(Flag::Z, result == 0);
+        self.set(dst, result);
         0
     }
 
     fn shift_logic(&mut self, dst: Bits8) -> u8 {
-        let mut data = self.get(dst);
-        let carry = (data & BIT0) != 0;
-        data >>= 1;
-        self.set(Flag::C, carry);
+        let data = self.get(dst);
+        let result = data >> 1;
+        self.set(Flag::C, (data & 1) == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        self.set(dst, data);
+        self.set(Flag::Z, result == 0);
+        self.set(dst, result);
         0
     }
 
     fn swap(&mut self, dst: Bits8) -> u8 {
         let data = self.get(dst);
-        let lower = data & LOWER_HALF;
-        let upper = data & UPPER_HALF;
-        let data = (lower << 4) | (upper >> 4);
+        let upper = data >> 4;
+        let lower = data << 4;
+        let data = lower | upper;
         self.set(Flag::Z, data == 0);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
@@ -71,43 +60,38 @@ impl Shift<Bits8> for Registers {
 }
 
 impl Shift<u8> for Registers {
-    fn shift_left(&mut self, mut data: u8) -> u8 {
-        let carry = (data & BIT7) != 0;
-        data <<= 1;
-        self.set(Flag::C, carry);
+    fn shift_left(&mut self, data: u8) -> u8 {
+        let result = data << 1;
+        let bit7 = data >> 7;
+        self.set(Flag::C, bit7 == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        data
+        self.set(Flag::Z, result == 0);
+        result
     }
 
-    fn shift_arithmetic(&mut self, mut data: u8) -> u8 {
-        let carry = (data & BIT0) != 0;
-        let sign = data & BIT7;
-        data &= !(BIT7);
-        data >>= 1;
-        data |= sign;
-        self.set(Flag::C, carry);
+    fn shift_arithmetic(&mut self, data: u8) -> u8 {
+        let result = data >> 1 | (data & 0x80);
+        self.set(Flag::C, (data & 1) == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        data
+        self.set(Flag::Z, result == 0);
+        result
     }
 
-    fn shift_logic(&mut self, mut data: u8) -> u8 {
-        let carry = (data & BIT0) != 0;
-        data >>= 1;
-        self.set(Flag::C, carry);
+    fn shift_logic(&mut self, data: u8) -> u8 {
+        let result = data >> 1;
+        self.set(Flag::C, (data & 1) == 1);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
-        self.set(Flag::Z, data == 0);
-        data
+        self.set(Flag::Z, result == 0);
+        result
     }
 
     fn swap(&mut self, data: u8) -> u8 {
-        let lower = data & LOWER_HALF;
-        let upper = data & UPPER_HALF;
-        let data = (lower << 4) | (upper >> 4);
+        let upper = data >> 4;
+        let lower = data << 4;
+        let data = lower | upper;
         self.set(Flag::Z, data == 0);
         self.set(Flag::H, false);
         self.set(Flag::N, false);
