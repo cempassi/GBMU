03138d806757164b4dd6722436c02c0d8b161651
Cedric M'PASSI <cempassi@student.42.fr>
2021-12-30 03:37:23 +0100

    Add disable bios mapping

--- crates/memory/src/state.rs
+++ crates/memory/src/state.rs
@@ -1,7 +1,16 @@
+use shared::Error;
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum State {
     Bios,
     Rom,
+}
+
+impl State {
+    pub fn disable_bios(&mut self) -> Result<(), Error> {
+        *self = Self::Rom;
+        println!("Disabling Bios");
+        Ok(())
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
